35848efa1dca5776a3b87dc71daf693fac991dec
rounakbhowmick <rounakchakda@gmail.com>
2020-11-08 16:45:42 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+//Import Routes
+const authRoute = require("./routes/auth");
+dotenv.config();
+
+//Connect to DB
+mongoose
+	.connect(process.env.DB_CONNECTION, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("Connected");
+	})
+	.catch((error) => {
+		console.log(error);
+	});
+
+//Middleware
+app.use(express.json());
+//Route Middlewares
+app.use("/api/user", authRoute);
+//app.listen(5000);
+const port = 5000;
+app.listen(port, () => {
+	console.log(`App ♻ running on port ${port}...`);
+});
